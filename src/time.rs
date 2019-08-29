63de3219d14868db31ac3d0d9c632229e9c7e168
Yang Keao <keao.yang@yahoo.com>
2019-08-29 17:12:21 +0800

    use stack variable rather than malloc

--- src/time.rs
+++ src/time.rs
@@ -1,30 +1,50 @@
 use crate::PosixTime;
-use libc::rusage;
-use libc::timespec;
-use std::mem::size_of;
+use libc::{rusage, timespec, c_long, time_t, suseconds_t, timeval};
 use std::time::Duration;
 
 fn get_r_usage() -> Result<Box<rusage>, libc::c_int> {
-    unsafe {
-        let r_usage = libc::malloc(size_of::<rusage>()) as *mut libc::rusage;
-        let errno = libc::getrusage(libc::RUSAGE_SELF, r_usage);
-        if errno != 0 {
-            Err(errno)
-        } else {
-            Ok(Box::from_raw(r_usage))
-        }
+    let mut r_usage = rusage {
+        ru_utime: timeval{ tv_sec: 0 as time_t, tv_usec: 0 as suseconds_t, },
+        ru_stime: timeval{ tv_sec: 0 as time_t, tv_usec: 0 as suseconds_t, },
+        ru_maxrss: 0 as c_long,
+        ru_ixrss: 0 as c_long,
+        ru_idrss: 0 as c_long,
+        ru_isrss: 0 as c_long,
+        ru_minflt: 0 as c_long,
+        ru_majflt: 0 as c_long,
+        ru_nswap: 0 as c_long,
+        ru_inblock: 0 as c_long,
+        ru_oublock: 0 as c_long,
+        ru_msgsnd: 0 as c_long,
+        ru_msgrcv: 0 as c_long,
+        ru_nsignals: 0 as c_long,
+        ru_nvcsw: 0 as c_long,
+        ru_nivcsw: 0 as c_long,
+    };
+ 
+    let errno = unsafe {
+       libc::getrusage(libc::RUSAGE_SELF, &mut r_usage as *mut rusage)
+    };
+
+    if errno != 0 {
+        Err(errno)
+    } else {
+        Ok(Box::new(r_usage))
     }
 }
 
 fn clock_gettime() -> Result<Box<timespec>, libc::c_int> {
-    unsafe {
-        let time_spec = libc::malloc(size_of::<timespec>()) as *mut libc::timespec;
-        let errno = libc::clock_gettime(libc::CLOCK_PROCESS_CPUTIME_ID, time_spec);
-        if errno != 0 {
-            Err(errno)
-        } else {
-            Ok(Box::from_raw(time_spec))
-        }
+    let mut time_spec = timespec {
+        tv_nsec: 0 as c_long,
+        tv_sec: 0 as time_t,
+    };
+    let errno = unsafe {
+        libc::clock_gettime(libc::CLOCK_PROCESS_CPUTIME_ID, &mut time_spec as *mut timespec)
+    };
+    if errno != 0 {
+        Err(errno)
+    } else {
+        Ok(Box::new(time_spec))
     }
 }
 
